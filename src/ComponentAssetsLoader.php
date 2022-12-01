574391aab74aaa3f9757af7d8bbc376ffa33f442
freekattema <freekattema@live.nl>
2022-12-01 14:15:06 +0100

    Added the wordpress theme buffer as a dependency

--- src/ComponentAssetsLoader.php
+++ src/ComponentAssetsLoader.php
@@ -2,6 +2,8 @@
 
 namespace Freekattema\Component;
 
+use Freekattema\WordpressThemeBuffer\Buffer;
+
 final class ComponentAssetsLoader
 {
     private function __construct() {}
@@ -10,7 +12,7 @@ final class ComponentAssetsLoader
     {
         $cssFile = '/dist/exports/' . $component . '.css';
         if (file_exists(get_template_directory() . $cssFile)) {
-            \ThemeBuffer::add_css(get_template_directory_uri() . $cssFile);
+            Buffer::add_css(get_template_directory_uri() . $cssFile);
         }
     }
 }
