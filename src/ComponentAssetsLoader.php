86e7d47de5952ad2dd692833aeebb94e1a898579
freekattema <freekattema@live.nl>
2022-12-01 14:04:39 +0100

    Initial

---
+++ src/ComponentAssetsLoader.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Freekattema\Component;
+
+final class ComponentAssetsLoader
+{
+    private function __construct() {}
+
+    public static function attach(string $component)
+    {
+        $cssFile = '/dist/exports/' . $component . '.css';
+        if (file_exists(get_template_directory() . $cssFile)) {
+            \ThemeBuffer::add_css(get_template_directory_uri() . $cssFile);
+        }
+    }
+}
